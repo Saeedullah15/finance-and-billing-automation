b056e5118f317587f2b1b33e88d31a52876fbbf1
Saeedullah15 <saeedullahazim65@gmail.com>
2023-11-15 00:18:25 +0600

    postgres database connection by typeorm done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pg16@23',
+      database: 'finance-and-billing-db',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
